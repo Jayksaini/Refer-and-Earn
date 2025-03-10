1b185fc43b41e5a3fa4ec21d4a82e45e2193a18d
Shobhit Rawat <161796939+shobhit902@users.noreply.github.com>
2025-03-10 19:51:46 +0530

    Remove md

--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -5,14 +5,17 @@ const Navbar = () => {
     <>
       <nav className="flex justify-between bg-black font-[roboto] text-white p-4 pt-8">
         <div>
-          <h1 className="font-bold text-2xl">ReferRewards</h1>
+          <h1 className="font-bold text-2xl text-[rgba(248, 242, 242, 1)] " >ReferRewards</h1>
           </div>
-          <ul className="hidden md:flex space-x-6">
+          <div>
+          <ul className="flex space-x-6">
             <li><a href="">Home</a></li>
             <li><a href="">My referrals</a></li>
             <li><a href=""><img src="" alt="profile" /></a></li>
           </ul>
        
+          </div>
+        
       </nav>
     </>
   );
